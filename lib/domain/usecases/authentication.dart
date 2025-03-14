c58b0cc931337b1a473b0ce846e61386dffa497f
Rafael Brito <henrique8264@gmail.com>
2025-03-13 21:28:22 -0300

    feat(domain): :sparkles: add Authentication usecase

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,12 @@
+import '../entities/entities.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({required this.email, required this.password});
+}
