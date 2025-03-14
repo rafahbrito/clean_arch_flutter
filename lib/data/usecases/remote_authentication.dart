553c490624b6a0a43384fdc69c72ecbd5b1255ec
Rafael Brito <henrique8264@gmail.com>
2025-03-14 20:05:39 -0300

    feat(data): :sparkles: implement remote authentication with HttpClient and params mapping

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,32 @@
+import 'package:clean_arch_flutter/data/http/http.dart';
+import 'package:clean_arch_flutter/domain/usecases/usecases.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({required this.httpClient, required this.url});
+
+  Future<void> auth(AuthenticationParams params) async {
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: RemoteAuthenticationParams.fromDomain(params).toJson(),
+    );
+  }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({required this.email, required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+        email: params.email,
+        password: params.password,
+      );
+
+  Map<String, String> toJson() => {'email': email, 'password': password};
+}
