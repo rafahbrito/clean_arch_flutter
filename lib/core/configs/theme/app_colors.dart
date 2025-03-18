2f487b2586c50651688308febba7e2b0565b690f
Rafael Brito <henrique8264@gmail.com>
2025-03-18 14:37:46 -0300

    feat(core): :sparkles: add app colors and theme

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFE21221);
+  static const background = Color(0xFF181A20);
+  static const secondBackground = Color(0xFF35383F);
+}
