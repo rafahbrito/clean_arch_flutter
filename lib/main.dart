7e3858604b4b7448e41e080803c3d6d4479ef449
Rafael Brito <henrique8264@gmail.com>
2025-03-18 18:21:51 -0300

    feat(ui): :sparkles: feat: Implement splash screen and initial navigation logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:clean_arch_flutter/core/configs/theme/app_theme.dart';
+import 'package:clean_arch_flutter/ui/screens/splash/bloc/splash_cubit.dart';
 import 'package:clean_arch_flutter/ui/screens/splash/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.appTheme,
-      home: SplashScreen(),
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
+        home: SplashScreen(),
+      ),
     );
   }
 }
--- lib/ui/screens/splash/splash.dart
+++ lib/ui/screens/splash/splash.dart
@@ -1,5 +1,11 @@
 import 'package:clean_arch_flutter/core/configs/assets/app_images.dart';
+import 'package:clean_arch_flutter/core/configs/navigation/app_navigation.dart';
+import 'package:clean_arch_flutter/ui/screens/auth/sign_in.dart';
+import 'package:clean_arch_flutter/ui/screens/home/home.dart';
+import 'package:clean_arch_flutter/ui/screens/splash/bloc/splash_cubit.dart';
+import 'package:clean_arch_flutter/ui/screens/splash/bloc/splash_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,9 +13,19 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(AppImages.splashBg)),
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is Unauthenticated) {
+            AppNavigator.pushReplacement(context, SignInScreen());
+          }
+          if (state is Authenticated) {
+            AppNavigator.pushReplacement(context, HomeScreen());
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(AppImages.splashBg)),
+          ),
         ),
       ),
     );
