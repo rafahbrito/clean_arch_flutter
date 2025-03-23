d6a978f6a85d17a84cf2dcdfade85c9c5686346b
Rafael Brito <henrique8264@gmail.com>
2025-03-23 16:51:06 -0300

    reset business logic

--- lib/data/http/http_client.dart
+++
@@ -1,8 +0,0 @@
-abstract class HttpClient {
-  Future<dynamic> request({
-    required String url,
-    required String method,
-    Map<String, dynamic>? body,
-    Map<String, String>? headers,
-  });
-}
