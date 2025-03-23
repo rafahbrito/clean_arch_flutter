e3086b7a961148e7161febed0c16d839909394be
Rafael Brito <henrique8264@gmail.com>
2025-03-23 16:47:49 -0300

    feat(ui): :sparkles: add SignUp Screen

--- lib/ui/screens/auth/sign_up.dart
+++ lib/ui/screens/auth/sign_up.dart
@@ -1,3 +1,7 @@
+import 'package:clean_arch_flutter/core/configs/navigation/app_navigation.dart';
+import 'package:clean_arch_flutter/ui/screens/auth/sign_in.dart';
+import 'package:clean_arch_flutter/ui/widgets/async_button/button.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -5,6 +9,71 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        minimum: EdgeInsets.only(top: 100, right: 16, left: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _signUpText(),
+            SizedBox(height: 30),
+            _emailField(),
+            SizedBox(height: 20),
+            _passwordField(),
+            SizedBox(height: 60),
+            AsyncButton(
+              onPressed: () async {},
+              onSuccess: () {},
+              onFailure: (error) {},
+            ),
+            SizedBox(height: 20),
+            _signInText(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpText() {
+    return Text(
+      "Criar Conta",
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+    );
+  }
+
+  Widget _emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(hintText: 'Email'),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Senha',
+        suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.visibility)),
+      ),
+    );
+  }
+
+  Widget _signInText(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(text: "Já possui uma conta? "),
+          TextSpan(
+            text: "Entrar",
+            style: TextStyle(color: Colors.blueAccent),
+            recognizer:
+                TapGestureRecognizer()
+                  ..onTap = () {
+                    AppNavigator.push(context, SignInScreen());
+                  },
+          ),
+        ],
+      ),
+    );
   }
 }
