383c98fb016c663d79149b81e1f254282d1c33f6
Rafael Brito <henrique8264@gmail.com>
2025-03-23 17:01:03 -0300

    feat: add service locator setup with GetIt and registers DioClient

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch_flutter/core/configs/theme/app_theme.dart';
+import 'package:clean_arch_flutter/service_locator.dart';
 import 'package:clean_arch_flutter/ui/screens/splash/bloc/splash_cubit.dart';
 import 'package:clean_arch_flutter/ui/screens/splash/splash.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
---
+++ lib/service_locator.dart
@@ -0,0 +1,8 @@
+import 'package:clean_arch_flutter/core/network/dio_client.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void setupServiceLocator() {
+  sl.registerSingleton<DioClient>(DioClient());
+}
